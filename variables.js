2fbc8c672451ba2fdbdc489c55a0fef5ac5b2167
jojo <jblanc@simplon.co>
2021-02-08 19:51:31 +0100

    first

---
+++ variables.js
@@ -0,0 +1,58 @@
+// LES VARIABLES
+
+// ------------------------EXO 1------------------------
+// Assigner la valeur Ford à la variable voiture.
+// Assigner la valeur 5 à la variable etoiles
+// Doit apparaitre dans la console : Ford 5
+
+// ------------------------EXO 2------------------------
+//Le nom des variables : const age; const note; const nom; const jojo; const quoi;
+// Doit apparaitre dans la console : 18 -7 "Manu" false undefined
+
+// ------------------------EXO 3------------------------
+// Le nom des variables : let x, let y, let z;
+// Stockez des valeurs à vos variables
+// Vous devez passer par cette étape : x = x * 10; y = y / 2; z = x + y;
+// Doit apparaitre dans la console : 80 4 84
+
+// ------------------------EXO 4------------------------
+// Compléter la phrase en assignant des valeurs à ces 3 variables :
+// let nomSas = Hackeuse; let duree = 7; let formationPro = "développeuse web";
+// Concaténer
+// Doit apparaitre dans la console : Je participe au sas Hackeuse pendant 7 semaines afin d'intégrer la formation développeuse web.
+
+// ------------------------EXO 5------------------------
+// Afficher le nombre de caractères de la variable nom avec la propriété length
+// let longueurDuNom;
+// let preNom = 'Grace Hopper';
+// longueurDuNom = preNom;
+// Doit apparaitre dans la console le chiffre 12
+
+// ------------------------EXO 6------------------------
+// Afficher la troisième et la dernière lettre de la variable nom avec le système de brackets.
+// let nom = 'Margaret';
+// let troisiemeLettre = nom;
+// let derniereLettre = nom;
+// Doit appraitre dans la console : r t
+
+// ------------------------EXO 7------------------------
+// Créer un tableau avec la variable myArray.
+// Le tableau devrait comporter un string, un number négatif, une division et un test.
+// Doit apparaitre dans la console : ["bibiche", -5, 4, false]
+
+// ------------------------EXO 8------------------------
+// myData devrait être un tableau => [brackets]
+// myData devrait avoir comme valeurs : [12,6,8], [44,71,90], [23,54,99].
+// Changer la valeur 90 par 11.
+// Doit apparaitre dans la console : 11 23
+
+// ------------------------EXO 9------------------------
+// Afin de réaliser correctement l'exercice, vous allez avoir besoin des notions suivantes : les méthodes push(),pop(), shift(), unshift(), splice()
+// Commencer par écrire le tableau suivant : let friends = [ 'Janice', 'Monica', 'Ben' ];
+// Supprimer 'Ben'
+// Supprimer 'Janice'
+// Ajouter à la fin du tableau 'Phoebe'.
+// Ajouter au début du tableau 'Rachel'.
+// Placer 'Ross', 'Chandler' et 'Joey' entre 'Monica' et 'Phoebe'.
+// Doit apparaitre dans la console : (6) ["Rachel", "Monica", "Ross", "Chandler",
+// "Joey", "Phoebe"]
