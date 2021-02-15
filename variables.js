287a5929c1376f47589afc783270707e08f8934d
tutu <jonathantan75020@gmail.com>
2021-02-15 12:20:36 +0100

    last

--- variables.js
+++ variables.js
@@ -5,40 +5,95 @@
 // Assigner la valeur 5 à la variable etoiles
 // Doit apparaitre dans la console : Ford 5
 
+// <===== DEBUT CODE =====>
+// let voiture;
+// let etoiles;
+// console.log();
+// <===== FIN CODE =====>
+
 // ------------------------EXO 2------------------------
-//Le nom des variables : const age; const note; const nom; const jojo; const quoi;
+// Assigner les bonnes valeurs aux variables suivantes :
 // Doit apparaitre dans la console : 18 -7 "Manu" false undefined
 
+// <===== DEBUT CODE =====>
+// let age;
+// let note;
+// let nom;
+// let jojo;
+// let quoi;
+// console.log();
+// <===== FIN CODE =====>
+
 // ------------------------EXO 3------------------------
-// Le nom des variables : let x, let y, let z;
-// Stockez des valeurs à vos variables
-// Vous devez passer par cette étape : x = x * 10; y = y / 2; z = x + y;
+// Stockez les bonnes valeurs aux variables :
+
+// <===== DEBUT CODE =====>
+// let x;
+// let y
+// let z;
+// <===== FIN CODE =====>
+
+// Vous devez passer par cette étape :
+
+// <===== DEBUT CODE =====>
+// x = x * 10;
+// y = y / 2;
+// z = x + y;
+// <===== FIN CODE =====>
+
 // Doit apparaitre dans la console : 80 4 84
+
+// <===== DEBUT CODE =====>
+// console.log()
+// <===== FIN CODE =====>
 
 // ------------------------EXO 4------------------------
 // Compléter la phrase en assignant des valeurs à ces 3 variables :
-// let nomSas = Hackeuse; let duree = 7; let formationPro = "développeuse web";
 // Concaténer
 // Doit apparaitre dans la console : Je participe au sas Hackeuse pendant 7 semaines afin d'intégrer la formation développeuse web.
 
+// <===== DEBUT CODE =====>
+// let nomSas = 'Hackeuse';
+// let duree = 7;
+// let formationPro = 'développeuse web';
+// console.log();
+// <===== FIN CODE =====>
+
 // ------------------------EXO 5------------------------
 // Afficher le nombre de caractères de la variable nom avec la propriété length
+// Doit apparaitre dans la console le chiffre 12
+
+// <===== DEBUT CODE =====>
 // let longueurDuNom;
 // let preNom = 'Grace Hopper';
 // longueurDuNom = preNom;
-// Doit apparaitre dans la console le chiffre 12
+// console.log();
+// <===== FIN CODE =====>
 
 // ------------------------EXO 6------------------------
 // Afficher la troisième et la dernière lettre de la variable nom avec le système de brackets.
-// let nom = 'Margaret';
-// let troisiemeLettre = nom;
-// let derniereLettre = nom;
 // Doit appraitre dans la console : r t
 
+// <===== DEBUT CODE =====>
+// let idole = 'Margaret';
+// let troisiemeLettre = nom;
+// let derniereLettre = nom;
+// console.log();
+// <===== FIN CODE =====>
+
 // ------------------------EXO 7------------------------
-// Créer un tableau avec la variable myArray.
+// Créer un tableau avec la variable myArray
 // Le tableau devrait comporter un string, un number négatif, une division et un test.
 // Doit apparaitre dans la console : ["bibiche", -5, 4, false]
+
+// <===== DEBUT CODE =====>
+// let myArray;
+// let;
+// let;
+// let;
+// let;
+// console.log();
+// <===== FIN CODE =====>
 
 // ------------------------EXO 8------------------------
 // myData devrait être un tableau => [brackets]
@@ -46,9 +101,16 @@
 // Changer la valeur 90 par 11.
 // Doit apparaitre dans la console : 11 23
 
+// <===== DEBUT CODE =====>
+// let myData;
+// myData;
+// console.log();
+// <===== FIN CODE =====>
+
 // ------------------------EXO 9------------------------
 // Afin de réaliser correctement l'exercice, vous allez avoir besoin des notions suivantes : les méthodes push(),pop(), shift(), unshift(), splice()
-// Commencer par écrire le tableau suivant : let friends = [ 'Janice', 'Monica', 'Ben' ];
+// Voici le tableau à modifier :
+// let friends = [ 'Janice', 'Monica', 'Ben' ];
 // Supprimer 'Ben'
 // Supprimer 'Janice'
 // Ajouter à la fin du tableau 'Phoebe'.
@@ -57,43 +119,58 @@
 // Doit apparaitre dans la console : (6) ["Rachel", "Monica", "Ross", "Chandler",
 // "Joey", "Phoebe"]
 
+// <===== DEBUT CODE =====>
+// let friends = [ 'Janice', 'Monica', 'Ben' ];
+// friends;
+// friends;
+// friends;
+// friends;
+// friends;
+// console.log();
+// <===== FIN CODE =====>
+
 // ------------------------EXO 10------------------------
 // La société Simplon.co a demandé à ta collègue Ada de rédiger un morceau de code qui sera utilisé pour la com.
 // Ce code permettra de connaître la prochaine date de formation.
 // Ada a utilisé la variable dateFormation pour stocker la date mais son code ne fonctionne pas.
 // Aide là à trouver la solution.
 
+// <===== DEBUT CODE =====>
 // let dateFormation = "16 février 2021";
-
 // console.log(`La prochaine formation commencera à partir du ${dateformation}`);
+// <===== FIN CODE =====>
 
 // ------------------------EXO 11------------------------
 // La société Simplon.co demande à ta collègue Ada de calculer la somme du nombre de like du dernier post Twitter du jour et de la veille.
 // Ada est proche du but mais son code ne fonctionne pas.
 // Aide la à trouver la solution.
 
+// <===== DEBUT CODE =====>
 // let likeHier = "245";
 // let likeAujourdhui = 321;
 // let totalLike = likeHier + likeAujourdhui;
-
 // console.log(totalLike);
+// <===== FIN CODE =====>
 
 // ------------------------EXO 12------------------------
 // La société Simplon.co demande à ta collègue Ada de créer une réunion dans le planning avec l'heure de début.
 // Ada a laissé le stagiaire s'en occuper mais il n'y arrive pas.
 // Aide le à trouver la solution.
 
+// <===== DEBUT CODE =====>
 // let h = 10;
 // let m = 45;
 // let separateur = h;
 // let debutReunion = H + separateur + M;
-
 // console.log(`La réunion commence à ${debutReunion}`);
+// <===== FIN CODE =====>
 
 // ------------------------EXO 13------------------------
 // Afficher dans la console l'opération de 2 * 4 + 6, le tout divisé par 2.
 
+// <===== DEBUT CODE =====>
 // console.log();
+// <===== FIN CODE =====>
 
 // ------------------------EXO 14------------------------
 // La société Simplon.co a besoin d'une estimation du coût d'achats de cadeaux pour les partenaires.
@@ -101,14 +178,18 @@
 // L'achat et l'envoi d'un cadeau coûte en tout 5€35.
 // Aide Ada a calculer le prix final de l'opération.
 
+// <===== DEBUT CODE =====>
 // console.log();
+// <===== FIN CODE =====>
 
 // ------------------------EXO 15------------------------
 // Le budget maximum étant de 1500€, Ada a donc cherché une autre entreprise et a finalement trouvé une autre offre à 3€20.
 // Par contre, cette entreprise fait facturer un supplément de 500€ pour les taxes.
 // Aide Ada a calculer le prix final de la nouvelle offre.
 
+// <===== DEBUT CODE =====>
 // console.log();
+// <===== FIN CODE =====>
 
 // ------------------------EXO 16------------------------
 // Pour financer la nouvelle campagne marketing, Ada organise une levée de fonds et fait appel à des investisseurs et des amis.
@@ -116,4 +197,6 @@
 // Ada a récolté 1328€ des investisseurs et 565€ de ses amis.
 // Ada te demande de l'aider à calculer le bénéfice. Les bénéfices sont les sommes récoltées moins les taxes.
 
+// <===== DEBUT CODE =====>
 // console.log();
+// <===== FIN CODE =====>
