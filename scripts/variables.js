ddf06a4cb4f6470ba5f4435cce29e3a9043876b8
tutu <jonathantan75020@gmail.com>
2021-02-18 12:31:02 +0100

    var0

--- scripts/variables.js
+++ scripts/variables.js
@@ -7,9 +7,9 @@ console.log('----------1.Les variables----------');
 // Doit apparaitre dans la console : Ford 5
 
 // <===== DEBUT CODE =====>
-let voiture = 'Ford';
-let etoiles = 5;
-console.log(voiture, etoiles);
+// let voiture;
+// let etoiles;
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 2------------------------
@@ -17,35 +17,35 @@ console.log(voiture, etoiles);
 // Doit apparaitre dans la console : 18 -7 "Manu" false undefined
 
 // <===== DEBUT CODE =====>
-let age = 18;
-let note = -7;
-let nom = 'Manu';
-let jojo = 3 == 5;
-let quoi;
-console.log(age, note, nom, jojo, quoi);
+// let age;
+// let note;
+// let nom;
+// let jojo;
+// let quoi;
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 3------------------------
 // Stockez les bonnes valeurs aux variables :
 
 // <===== DEBUT CODE =====>
-let x = 8;
-let y = 8;
-let z;
+// let x;
+// let y;
+// let z;
 // <===== FIN CODE =====>
 
 // Vous devez passer par cette étape :
 
 // <===== ENLEVER LES COMMENTAIRES =====>
-x = x * 10;
-y = y / 2;
-z = x + y;
+// x = x * 10;
+// y = y / 2;
+// z = x + y;
 // <===== ENLEVER LES COMMENTAIRES =====>
 
 // Doit apparaitre dans la console : 80 4 84
 
 // <===== DEBUT CODE =====>
-console.log(x, y, z);
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 4------------------------
@@ -54,10 +54,10 @@ console.log(x, y, z);
 // Doit apparaitre dans la console : Je participe au sas Hackeuse pendant 7 semaines afin d'intégrer la formation développeuse web.
 
 // <===== DEBUT CODE =====>
-let nomSas = 'Artis';
-let duree = 7;
-let formationPro = 'développeuse DATA';
-console.log(`Je participe au sas ${nomSas} pendant ${duree} semaines afin d'intégrer la formation ${formationPro}.`);
+// let nomSas = 'Hackeuses';
+// let duree = 7;
+// let formationPro = 'développeuse Web';
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 5------------------------
@@ -65,10 +65,10 @@ console.log(`Je participe au sas ${nomSas} pendant ${duree} semaines afin d'int�
 // Doit apparaitre dans la console le chiffre 12
 
 // <===== DEBUT CODE =====>
-let longueurDuNom;
-let preNom = 'Grace Hopper';
-longueurDuNom = preNom.length;
-console.log(longueurDuNom);
+// let longueurDuNom;
+// let preNom = 'Grace Hopper';
+// longueurDuNom = preNom;
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 6------------------------
@@ -77,9 +77,9 @@ console.log(longueurDuNom);
 
 // <===== DEBUT CODE =====>
 let idole = 'Margaret';
-let troisiemeLettre = idole[2];
-let derniereLettre = idole[idole.length - 1];
-console.log(troisiemeLettre, derniereLettre);
+let troisiemeLettre;
+let derniereLettre;
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 7------------------------
@@ -88,8 +88,8 @@ console.log(troisiemeLettre, derniereLettre);
 // Doit apparaitre dans la console : ["bibiche", -5, 4, false]
 
 // <===== DEBUT CODE =====>
-let myArray = [ 'bibiche', -5, 8 / 2, 3 > 22 ];
-console.log(myArray);
+// let myArray;
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 8------------------------
@@ -99,9 +99,9 @@ console.log(myArray);
 // Doit apparaitre dans la console : 11 23
 
 // <===== DEBUT CODE =====>
-let myData = [ [ 12, 6, 8 ], [ 44, 71, 90 ], [ 23, 54, 99 ] ];
-myData[1][2] = 11;
-console.log(myData[1][2], myData[2][0]);
+// let myData;
+// myData;
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 9------------------------
@@ -116,13 +116,13 @@ console.log(myData[1][2], myData[2][0]);
 // Doit apparaitre dans la console : (6) ["Rachel", "Monica", "Ross", "Chandler", "Joey", "Phoebe"]
 
 // <===== DEBUT CODE =====>
-let friends = [ 'Janice', 'Monica', 'Ben' ];
-friends.pop();
-friends.shift();
-friends.push('Phoebe');
-friends.unshift('Rachel');
-friends.splice(2, 0, 'Ross', 'Chandler', 'Joey');
-console.log(friends);
+// let friends = [ 'Janice', 'Monica', 'Ben' ];
+// friends;
+// friends;
+// friends;
+// friends;
+// friends;
+// console.log(friends);
 // <===== FIN CODE =====>
 
 // ------------------------EXO 10------------------------
@@ -132,8 +132,8 @@ console.log(friends);
 // Aide là à trouver la solution.
 
 // <===== DEBUT CODE =====>
-let dateFormation = '16 février 2021';
-console.log(`La prochaine formation commencera à partir du ${dateFormation}`);
+// let dateFormation = '16 février 2021';
+// console.log(`La prochaine formation commencera à partir du ${dateformation}`);
 // <===== FIN CODE =====>
 
 // ------------------------EXO 11------------------------
@@ -142,10 +142,10 @@ console.log(`La prochaine formation commencera à partir du ${dateFormation}`);
 // Aide la à trouver la solution.
 
 // <===== DEBUT CODE =====>
-let likeHier = 245;
-let likeAujourdhui = 321;
-let totalLike = likeHier + likeAujourdhui;
-console.log(totalLike);
+// let likeHier = "245";
+// let likeAujourdhui = 321;
+// let totalLike = likeHier + likeAujourdhui;
+// console.log(totalLike);
 // <===== FIN CODE =====>
 
 // ------------------------EXO 12------------------------
@@ -154,18 +154,18 @@ console.log(totalLike);
 // Aide le à trouver la solution.
 
 // <===== DEBUT CODE =====>
-let h = 10;
-let m = 45;
-let separateur = 'h';
-let debutReunion = h + separateur + m;
-console.log(`La réunion commence à ${debutReunion}`);
+// let h = 10;
+// let m = 45;
+// let separateur = h;
+// let debutReunion = H + separateur + M;
+// console.log(`La réunion commence à ${debutReunion}`);
 // <===== FIN CODE =====>
 
 // ------------------------EXO 13------------------------
 // Afficher dans la console l'opération de 2 * 4 + 6, le tout divisé par 2.
 
 // <===== DEBUT CODE =====>
-console.log((2 * 4 + 6) / 2);
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 14------------------------
@@ -175,7 +175,7 @@ console.log((2 * 4 + 6) / 2);
 // Aide Ada a calculer le prix final de l'opération.
 
 // <===== DEBUT CODE =====>
-console.log((245 + 38) * 5.35);
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 15------------------------
@@ -184,7 +184,7 @@ console.log((245 + 38) * 5.35);
 // Aide Ada a calculer le prix final de la nouvelle offre.
 
 // <===== DEBUT CODE =====>
-console.log((245 + 38) * 3.2 + 500);
+// console.log();
 // <===== FIN CODE =====>
 
 // ------------------------EXO 16------------------------
@@ -194,5 +194,5 @@ console.log((245 + 38) * 3.2 + 500);
 // Ada te demande de l'aider à calculer le bénéfice. Les bénéfices sont les sommes récoltées moins les taxes.
 
 // <===== DEBUT CODE =====>
-console.log(1328 - 1328 * 5.5 / 100 + 565);
+// console.log();
 // <===== FIN CODE =====>
